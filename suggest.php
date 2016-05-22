44d4bd927a72ed202dc8b4fd149d726fc0b4bcf3
Matt English <menglish78@bellsouth.net>
2016-05-22 17:25:44 -0400

    Added a form to submit data.  Added process.php file to process the data.  Made it display results better.

--- suggest.php
+++ suggest.php
@@ -6,7 +6,27 @@ $section = "suggest";
 include("inc/header.php"); ?>
 
 <div class="section page">
-  <h1>Suggest a Media Item</h1>
+  <div class="wrapper">
+    <h1>Suggest a Media Item</h1>
+    <p>Think I&rsquo;ve missed something? Let me know! Complete the form to send me an email.</p>
+    <form method="post" action="process.php">
+      <table>
+        <tr>
+          <th><label for ="name">Name</lable></th>
+          <td><input type="text" id ="name" name="name"/></td>
+        </tr>
+        <tr>
+          <th><label for ="email">Email</lable></th>
+          <td><input type="text" id ="email" name="email"/></td>
+        </tr>
+        <tr>
+          <th><label for ="details">Suggest Item Details</label></th>
+          <td><textarea name ="details" id="details"></textarea></td>
+        </tr>
+      </table>
+      <input type="submit" value="Send" />
+    </form>
+  </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
